e4ce20090d82007af690cca65f1ef131a03e44a4
dannwise <wise.dann@gmail.com>
2019-07-09 10:56:40 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,7 +6,7 @@ def palindrome(string):
     # Making shure capitalized letters won't mess results up
     string = string.lower()
 
-    # Spliting string in half or integer part of a odd string and comparing equidistant terms
+    # Splitting string in half or integer part of a odd string and comparing equidistant terms
     for i in range(len(string)//2):
         if not(string[i] == string[-(i+1)]):
             editions += 1
